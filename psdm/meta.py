25ae75edd21466be8a7b81cc526333aa51c183b9
Sasan Jacob Rasti <sasan_jacob.rasti@tu-dresden.de>
2023-11-01 14:48:22 +0100

    BREAKING CHANGE: remove `meta.name` (contributors: @sasanjac)

--- psdm/meta.py
+++ psdm/meta.py
@@ -6,15 +6,11 @@ from __future__ import annotations
 
 import datetime as dt
 import enum
-import typing as t
 import uuid
 
 import pydantic
 
 from psdm.base import Base
-from psdm.base import model_validator_after
-from psdm.base import model_validator_before
-from psdm.base import validate_deprecated
 
 VERSION = "1.9.0"
 
@@ -27,25 +23,7 @@ class SignConvention(enum.Enum):
 class Meta(Base):
     """This class represents the meta data related to the grid export."""
 
-    name: str | None = pydantic.Field(None, repr=False)
-
-    @model_validator_after
-    def _validate_deprecated(self) -> Meta:
-        return validate_deprecated(self, attr_dpr="name", attr_new="grid")
-
-    grid: str | None = None
-
-    @model_validator_before
-    def _validate_naming(cls, data: t.Any) -> t.Any:  # noqa: ANN401
-        if data.get("grid") is None and data.get("name") is None:
-            msg = "grid\nField required"
-            raise ValueError(msg)
-
-        if data.get("grid") is None:
-            data["grid"] = data["name"]
-
-        return data
-
+    grid: str
     date: dt.date
 
     id: uuid.UUID = pydantic.Field(default_factory=uuid.uuid4)  # noqa: A003
