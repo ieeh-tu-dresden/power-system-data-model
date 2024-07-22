9fe150d276b3343c6214b83de7959e01bb14093c
SebastianDD <43751982+SebastianDD@users.noreply.github.com>
2024-07-22 12:07:07 +0200

    feat: validate topology case (contributors: @SebastianDD)

--- psdm/topology_case/case.py
+++ psdm/topology_case/case.py
@@ -4,10 +4,17 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+from loguru import logger
+
 from psdm.base import Base
 from psdm.base import UniqueTuple
 from psdm.meta import Meta
 from psdm.topology_case.element_state import ElementState
+
+if TYPE_CHECKING:
+    from psdm.topology.topology import Topology
 
 
 class Case(Base):
@@ -18,3 +25,27 @@ class Case(Base):
 
     meta: Meta
     elements: UniqueTuple[ElementState]
+
+    def is_valid_topology(self, topology: Topology) -> bool:
+        logger.info("Verifying topology case ...")
+        if topology.meta != self.meta:
+            logger.error("Metadata does not match.")
+            return False
+
+        if not self._is_proper_elements(topology):
+            return False
+
+        logger.info("Verifying topology case was successful.")
+        return True
+
+    def _is_proper_elements(self, topology: Topology) -> bool:
+        topology_elements = (
+            topology.loads + topology.transformers + topology.nodes + topology.branches + topology.external_grids
+        )
+        topology_element_names = [e.name for e in topology_elements]
+        for e in self.elements:
+            if e.name not in topology_element_names:
+                logger.error("Element {element_name} is not in topology.", element_name=e.name)
+                return False
+
+        return True
