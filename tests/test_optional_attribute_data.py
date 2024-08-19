9cd0ae6627331ed56653c83fbeda41c1b6847b93
KrahmerTUD <sebastian.krahmer@tu-dresden.de>
2024-08-19 17:45:58 +0200

    fix test for value is single AttributeData

--- tests/test_optional_attribute_data.py
+++ tests/test_optional_attribute_data.py
@@ -21,35 +21,35 @@ class TestAttributeData:
             (1.1, does_not_raise()),
             (True, does_not_raise()),
             ("value", does_not_raise()),
-            ((1, 1), does_not_raise()),
-            ((1, "value"), does_not_raise()),
-            ((), does_not_raise()),
+            ([1, 1], does_not_raise()),
+            ([1, "value"], does_not_raise()),
+            ([], does_not_raise()),
             (None, pytest.raises(pydantic.ValidationError)),
             (
                 {"key": "value"},
                 pytest.raises(pydantic.ValidationError),
             ),  # dict is forbidden as is not hashable
-            ((1, {"key": "value"}), pytest.raises(pydantic.ValidationError)),
+            ([1, {"key": "value"}], pytest.raises(pydantic.ValidationError)),
             (
                 AttributeData(name="test2", value=1),
                 pytest.raises(pydantic.ValidationError),
             ),
             (
-                (AttributeData(name="test2", value=2)),
-                does_not_raise(),  # TODO: should not raise an error
-            ),
-            (
-                (
-                    AttributeData(name="test2", value=2),
-                    AttributeData(name="test3", value=3),
-                ),
+                [AttributeData(name="test2", value=2)],
                 does_not_raise(),
             ),
             (
-                (
+                [
+                    AttributeData(name="test2", value=2),
+                    AttributeData(name="test3", value=3),
+                ],
+                does_not_raise(),
+            ),
+            (
+                [
                     AttributeData(name="test2", value=2),
                     AttributeData(name="test2", value=2),
-                ),
+                ],
                 pytest.raises(pydantic.ValidationError),  # not unique
             ),
             (
@@ -57,7 +57,9 @@ class TestAttributeData:
                     AttributeData(name="test2", value=2),
                     "string",
                 ),
-                pytest.raises(pydantic.ValidationError),  # not tuple of AttributeData
+                pytest.raises(
+                    pydantic.ValidationError,
+                ),  # not tuple of AttributeData only
             ),
         ],
     )
